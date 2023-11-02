e5b793966e5e5e327f861a7908d981f64aa02cde
jhjang7408 <jhjang7408@naver.com>
2023-11-02 18:09:05 +0900

    first commit

---
+++ src/main/java/com/secret/dessertmap/DessertmapApplication.java
@@ -0,0 +1,13 @@
+package com.secret.dessertmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DessertmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DessertmapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/secret/dessertmap/DessertmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secret.dessertmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DessertmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
